9617df187caa0abab35ea2e2804c4ba4619f5083
Santosh Marepally <santosh.marepally@gmail.com>
2020-11-11 20:55:53 +0000

    added VPC module

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/skm050-training/network/google"
+  version = "2.5.0"
+  # insert required variables here
+  network_name = "skm050 network"
+  project_id = var.project
+    subnets = [
+    {
+        subnet_name   = "skm050-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+    }
+    ]
+}
