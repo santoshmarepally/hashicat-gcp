c9de28cbb9a1e1e1fe08d68a7d5eba3e38de30a4
Santosh Marepally <santosh.marepally@gmail.com>
2020-11-11 21:03:57 +0000

    added VPC module

--- vpc.tf
+++ vpc.tf
@@ -2,11 +2,11 @@ module "network" {
   source  = "app.terraform.io/skm050-training/network/google"
   version = "2.5.0"
   # insert required variables here
-  network_name = "skm050-network"
+  network_name = "skm050training-network"
   project_id = var.project
     subnets = [
     {
-        subnet_name   = "skm050-subnet"
+        subnet_name   = "skm050training-subnet"
         subnet_ip     = "10.100.10.0/24"
         subnet_region = var.region
     }
